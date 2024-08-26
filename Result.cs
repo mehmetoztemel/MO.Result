559782f9b64c9957935029f943930acc2f559f16
Mehmet Öztemel <mehmet-oztemel@hotmail.com>
2024-08-26 23:47:12 +0300

    Update MO.Result.csproj

--- Result.cs
+++ Result.cs
@@ -1,52 +1,24 @@
-﻿using System.Text.Json.Serialization;
-
-namespace MO.Result
+﻿namespace MO.Result
 {
     public sealed class Result<T>
     {
-        public T? Data { get; private set; }
-        public bool IsSuccess { get; private set; }
-        public List<string>? ErrorMessages { get; private set; }
-
-        [JsonIgnore]
-        public int StatusCode { get; private set; }
-
-        private Result(T data)
+        public T? Data { get; set; }
+        public bool IsSuccess => ErrorMessages == null || ErrorMessages.Count == 0;
+        public List<string>? ErrorMessages { get; set; }
+        public int StatusCode { get; set; }
+        public static Result<T> Success(T data, int statusCode = 200)
         {
-            Data = data;
-            StatusCode = 200;
-            IsSuccess = true;
-        }
-
-        private Result(string errorMessage, int statusCode)
-        {
-            ErrorMessages = new List<string> { errorMessage };
-            StatusCode = statusCode;
-            IsSuccess = false;
-            Data = default;
-        }
-
-        private Result(List<string> errorMessages, int statusCode)
-        {
-            ErrorMessages = errorMessages;
-            StatusCode = statusCode;
-            IsSuccess = false;
-            Data = default;
-        }
-
-        public static Result<T> Success(T data)
-        {
-            return new Result<T>(data);
-        }
-
-        public static Result<T> Failure(string errorMessage, int statusCode = 500)
-        {
-            return new Result<T>(errorMessage, statusCode);
+            return new Result<T> { Data = data, StatusCode = statusCode };
         }
 
         public static Result<T> Failure(List<string> errorMessages, int statusCode = 500)
         {
-            return new Result<T>(errorMessages, statusCode);
+            return new Result<T> { ErrorMessages = errorMessages, StatusCode = statusCode };
+        }
+
+        public static Result<T> Failure(string errorMessage, int statusCode = 500)
+        {
+            return new Result<T> { ErrorMessages = new List<string> { errorMessage }, StatusCode = statusCode };
         }
     }
 }
